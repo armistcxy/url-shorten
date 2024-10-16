c8c00dcf8ef32edabe462cd071cd9cadd41ce2cf
Ngô Lê Hoàng <99264715+armistcxy@users.noreply.github.com>
2024-10-16 09:31:50 +0700

    Adding Benchmark for EncodeID and DecodeID (this 2 functions is used for generate shortenURL with backbone is K-V storage)

--- internal/domain/shorten_test.go
+++ internal/domain/shorten_test.go
@@ -51,3 +51,78 @@ func TestEncodeID(t *testing.T) {
 	randStr := "abc1Az24e"
 	assert.Equal(t, randStr, EncodeID(DecodeID(randStr)))
 }
+
+// Note that benchmark this way will create variety results, run about 4-5 times
+// to see typical result
+// Fix1: Because the result variety too much, I will use divide and conqueror range
+func benchmarkEncodeID(b *testing.B, min int64, max int64) {
+	var id string
+	nums := make([]int64, 1000)
+	for i := range nums {
+		nums[i] = rand.Int64N(max-min) + min
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		id = EncodeID(nums[i%1000])
+	}
+	runtime.KeepAlive(id)
+}
+
+// Number in [0, 10_000)
+func BenchmarkEncodeIDWithSmallNum(b *testing.B) {
+	benchmarkEncodeID(b, 0, 10_000)
+}
+
+// Number in [10_000, 1_000_000)
+func BenchmarkEncodeIDWithMediumNum(b *testing.B) {
+	benchmarkEncodeID(b, 10_000, 1_000_000)
+}
+
+// Number in [1_000_000, 1_000_000_000)
+func BenchmarkEncodeIDWithBigNum(b *testing.B) {
+	benchmarkEncodeID(b, 1_000_000, 1_000_000_000)
+}
+
+// Number in [1_000_000_000, 1_000_000_000_000)
+func BenchmarkEncodeIDWithSuperBigNum(b *testing.B) {
+	benchmarkEncodeID(b, 1_000_000_000, 1_000_000_000_000)
+}
+
+// Same problem with BenchmarkEncodeID
+// Note: Already fixed
+func benchmarkDecodeID(b *testing.B, len int) {
+	var num int64
+	ids := make([]string, 1000)
+	for i := range ids {
+		ids[i] = RandomString(len)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		num = DecodeID(ids[i%1000])
+	}
+	runtime.KeepAlive(num)
+}
+
+func BenchmarkDecodeIDWithLengthEqual1(b *testing.B) {
+	benchmarkDecodeID(b, 1)
+}
+
+func BenchmarkDecodeIDWithLengthEqual2(b *testing.B) {
+	benchmarkDecodeID(b, 2)
+}
+
+func BenchmarkDecodeIDWithLengthEqual3(b *testing.B) {
+	benchmarkDecodeID(b, 3)
+}
+
+func BenchmarkDecodeIDWithLengthEqual4(b *testing.B) {
+	benchmarkDecodeID(b, 4)
+}
+
+func BenchmarkDecodeIDWithLengthEqual5(b *testing.B) {
+	benchmarkDecodeID(b, 5)
+}
+
+func BenchmarkDecodeIDWithLengthEqual6(b *testing.B) {
+	benchmarkDecodeID(b, 6)
+}
