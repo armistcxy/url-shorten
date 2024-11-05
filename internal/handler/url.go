8595af288ba150f287ad64036376fea5d06f2bb3
Ngô Lê Hoàng <99264715+armistcxy@users.noreply.github.com>
2024-11-05 08:30:57 +0700

    Disable validate url

--- internal/handler/url.go
+++ internal/handler/url.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
 
@@ -72,21 +71,23 @@ func (uh *URLHandler) GetOriginURLHandle(w http.ResponseWriter, r *http.Request)
 func (uh *URLHandler) CreateShortURLHandle(w http.ResponseWriter, r *http.Request) {
 	form := CreateShortForm{}
 	if err := util.DecodeJSON(r, &form); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		slog.Error("fail when decode json body", "error", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// Currently disable cause fail when test
 	// Check if the URL is valid
-	if _, err := url.ParseRequestURI(form.Origin); err != nil {
-		http.Error(w, "Invalid URL", http.StatusBadRequest)
-		return
-	}
+	// if _, err := url.ParseRequestURI(form.Origin); err != nil {
+	// 	http.Error(w, "Invalid URL", http.StatusBadRequest)
+	// 	return
+	// }
 
 	id := uh.idGen.GenerateID()
 
 	short, err := uh.urlRepo.Create(context.Background(), id, form.Origin)
 	if err != nil {
+		slog.Error("failed to insert url to database", "error", err.Error())
 		http.Error(w, fmt.Sprintf("failed when creating short url, error: %s", err), http.StatusInternalServerError)
 		return
 	}
