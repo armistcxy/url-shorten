8da8b61811d044fbcee1ee983d7e947f10f304b7
ShinyAri <tanhngusi39@gmail.com>
2024-11-08 16:39:09 +0000

    Refactor: migrate App component to App.tsx and implement routing

---
+++ frontend/src/routes.ts
@@ -0,0 +1,15 @@
+import Home from "./pages/Home";
+import Preview from "./pages/Preview";
+
+const routes = [
+  {
+    path: "",
+    component: Home,
+  },
+  {
+    path: "/preview",
+    component: Preview,
+  },
+];
+
+export default routes;
