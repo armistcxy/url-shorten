2d68b9f4fceed58baacd1cefa1ea7fc5fc7edc59
Ngô Lê Hoàng <99264715+armistcxy@users.noreply.github.com>
2024-11-09 09:50:46 +0700

    remove unecessary file

--- internal/util/membership.go
+++
@@ -1,6 +0,0 @@
-package util
-
-// Use bloom filter technique for membership query
-// This will be the frontier to check whether URL was created before
-// if it returns false => 100% false
-// if it returns true => maybe true (i.e., false negative can occur)
